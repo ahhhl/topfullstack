d3fed7d3c23066e9c39610dbe1278529ce834b3d
ahhhl <46733275+ahhhl@users.noreply.github.com>
2024-03-27 08:21:24 +0000

    使用swagger和装饰器编写接口文档

--- server/apps/admin/src/main.ts
+++ server/apps/admin/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('接口API')
+    .setDescription('供后台管理界面调用的服务端API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
