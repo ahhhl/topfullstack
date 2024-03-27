dec2c3e81f4ae31cd51665549ff821ee6cbcc4a7
ahhhl <46733275+ahhhl@users.noreply.github.com>
2024-03-27 07:30:42 +0000

    使用Crud装饰器快速实现增删改查接口

--- server/libs/db/src/db.module.ts
+++ server/libs/db/src/db.module.ts
@@ -1,8 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { DbService } from './db.service';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { User } from './models/user.model';
 
+const models = TypegooseModule.forFeature([User]);
+
+@Global()
 @Module({
+  imports: [
+    TypegooseModule.forRoot('mongodb://localhost:7017/topfullstack', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+    }),
+    models,
+  ],
   providers: [DbService],
-  exports: [DbService],
+  exports: [DbService, models],
 })
 export class DbModule {}
